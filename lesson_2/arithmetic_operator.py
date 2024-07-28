2ec586309ffc40bfc6ec64a910f69290509c5e66
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-07-29 03:26:00 +0600

    add

--- lesson_2/arithmetic_operator.py
+++ lesson_2/arithmetic_operator.py
@@ -2,3 +2,5 @@ number_one=10
 number_two=5
 print(number_one + number_two)
 print(number_one * number_two)
+print(number_one / number_two)
+print(number_one // number_two)
---
+++ lesson_2/assingment_operator.py
@@ -0,0 +1,17 @@
+number=10
+number_two=20
+sun=number + number_two
+print(sun)
+
+
+num=10
+num_2=20
+num_3=5
+
+num +=num_2
+print("1st value",num)
+
+num -=num_3
+print("2nd value",num)
+num *=num_2
+print("3rd value",num)
