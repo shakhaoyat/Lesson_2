fdefab522f3b9cf91f2f4ed54bff93d6c489b1a2
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-07-29 03:37:25 +0600

    1

---
+++ lesson_2/compareison_operator.py
@@ -0,0 +1,12 @@
+massage="hello"
+massage_2="hello"
+print(massage==massage_2)
+
+number=10
+number_two=20
+
+print(number > number_two)
+print(number < number_two)
+print(number <= number_two)
+print(number >= number_two)
+print(number != number_two)
