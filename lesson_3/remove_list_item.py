2f9efa5f4d8d7daf1d1662edbef0286f81eac124
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-01 00:35:29 +0600

    1

---
+++ lesson_3/remove_list_item.py
@@ -0,0 +1,32 @@
+countries=["bangladesh","usa","india","newziland"]
+
+del countries[0]
+
+print(countries)
+
+#pop last item remove kore list thake
+hello=countries.pop()
+print(hello)
+
+print(countries)
+
+car=["a","b","c","d"]
+
+#change item of pop
+
+hel=car.pop(2)
+print(hel)
+print(car)
+
+
+vowel=["a","e","i","o","u"]
+
+vowel.remove('i')
+print(vowel)
+
+
+#if i use more then one then it remove 1st one
+
+vowel1=["a","e","i","o","u","i"]
+vowel1.remove('i')
+print(vowel1)
