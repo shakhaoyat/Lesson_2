fe79d3c67daf58ce70307546cc1aa0691d61b0c5
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-01 02:09:45 +0600

    1

---
+++ lesson_3/list_sort.py
@@ -0,0 +1,15 @@
+number=[3,5,1,2,7,0]
+fruits=["banana","apple","mango","orange"]
+number.sort()
+print(number)
+fruits.sort()
+print(fruits)
+
+
+
+
+
+another_number=[3,5,1,2,7,0]
+
+print(sorted(another_number))
+print(another_number)
