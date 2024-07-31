679599cc3f92af5589ed2ef0780ccabb813791ef
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-07-31 11:19:58 +0600

    new

---
+++ lesson_3/list.py
@@ -0,0 +1,10 @@
+car_model=["bmw","toyota","audi"]
+
+countries=["bangladesh","india","usa","china"]
+              0            1      2       3
+            -4           -3      -2      -1
+
+print(countries[0])
+
+#nagetive indexing star from last
+print(countries[-3])
