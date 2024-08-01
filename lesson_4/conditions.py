d355c16beaeb7a17f8e5b7065b49707ce7820614
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-02 02:17:24 +0600

    1

---
+++ lesson_4/conditions.py
@@ -0,0 +1,5 @@
+shirt_price=500
+if shirt_price==600:
+    print("I will buy")
+else:
+    print("i will return from market")    
---
+++ lesson_4/grade_marking.py
@@ -0,0 +1,18 @@
+#total mark greater then equal to 80 the A+
+#total mark greater then equal to 60 the less then 80 then gread B
+#total mark greater then equal to 40 the less then 60 then gread B
+#other wise gread f
+
+total_marks=42
+if total_marks >=80:
+    print("A+")
+elif total_marks >=60 and total_marks <80:
+    print("B")
+elif total_marks>=40 and total_marks<60:
+    print("C+")
+else:
+    print("F")
+
+
+
+
