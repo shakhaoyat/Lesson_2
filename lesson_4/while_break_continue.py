b330e74b5cedeb490e42753602580c33e142fb9f
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-06 01:02:16 +0600

    1

---
+++ lesson_4/while_break_continue.py
@@ -0,0 +1,22 @@
+i=1
+
+
+while i<=10:
+    if i==5:
+        print("stopping")
+        break
+    print(i)
+    i+=1
+
+print("____________")
+
+i = 1
+while i <= 10:
+    if i > 5 and i < 8:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+
+
---
+++ lesson_4/while_loop.py
@@ -0,0 +1,11 @@
+i=1
+"""
+while condition:
+    body of while loop
+"""
+
+while i<=10:
+    print(i)
+    i+=1
+
+print("stop")
