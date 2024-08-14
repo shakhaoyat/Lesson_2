2fd3f00c01f2027099a94e7f1341549a53ddb477
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-14 23:58:10 +0600

    1

---
+++ lesson_6/declare_tuple.py
@@ -0,0 +1,9 @@
+##fruits=("apple","banana","mango",1,2,1.5,["a","b","c"],"mango")
+fruits="apple","banana","mango",1,2,1.5,["a","b","c"],"mango"
+print(fruits)
+print(type(fruits))
+
+##1st bRAKET( )USE NA KORLE  striing hisabe dhorbe kintu onk item thakle ata ar hoi na
+
+name="chad"
+print(type(name))
