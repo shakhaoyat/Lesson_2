0332b8720eb42aebc31086bbe5b960ab906b688a
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-22 13:08:59 +0600

    8/22/24 new

--- lesson_6/looping_tuple.py
+++ lesson_6/looping_tuple.py
@@ -10,3 +10,7 @@ for i in range(1,len(fruits)):
     print(fruits[i])
 
 #range dia tuple dia range select kore dewa jai
+fruit_1=0
+while fruit_1 < len(fruits):
+    print(fruits[fruit_1])  # This will print each item in the tuple one by one
+    fruit_1 += 1  # Increment fruit_1 to move to the next item in the tuple
