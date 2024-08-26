054e0b93d76d53d75337972d7b1f12dcce10d9cd
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-26 15:27:33 +0600

    8/25/24

---
+++ lesson_6/accessing_dictonary.py
@@ -0,0 +1,15 @@
+employe={"name":"shakhaoyat","skill":["python","django","go"],
+         "experiance":4}
+print(employe["name"])
+print(employe["skill"])
+print(employe["experiance"])
+
+#print(employe["salary"])
+#ata eoror dekhabe karon salary nai employe te kintu get dia korle valoue na thakle none show kore
+
+
+#get()
+
+print(employe.get("name"))
+print(employe.get("salary"))
+
---
+++ lesson_6/declare_dictionary.py
@@ -0,0 +1,12 @@
+student={"name":"Shakhaoyat Hossain","Age":24,"cources":["python","java","c++"]}
+print(student)
+#empty dictionary
+empty_dictionary={}
+print(empty_dictionary)
+print(type(empty_dictionary))
+
+
+empty_dictionary2=dict()
+print(empty_dictionary2)
+print(type(empty_dictionary2))
+
