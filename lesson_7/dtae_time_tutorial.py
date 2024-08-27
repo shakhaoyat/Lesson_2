04d92b1857838cf337cb9835287a7c4be997acb0
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-28 00:04:27 +0600

    1

--- lesson_7/dtae_time_tutorial.py
+++ lesson_7/dtae_time_tutorial.py
@@ -3,7 +3,16 @@ import datetime
 print(datetime.datetime.now())
 #current_utc time
 print(datetime.datetime.utcnow())
+#get date
 
 print(datetime.date.today())
+#timestamp
 
-print(datetime.date.())
+print(datetime.date.fromtimestamp(123456789))
+random_date=datetime.date.fromtimestamp(123456789)
+print(type(random_date))
+print(random_date.year)
+print(random_date.month)
+print(random_date.day)
+
+
