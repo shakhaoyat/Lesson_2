3ed2ac2c64d1229639788f41868fefba85a18204
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-08-29 00:47:50 +0600

    1

---
+++ attribute_of_class.py
@@ -0,0 +1,30 @@
+class Person:
+    def __init__(self,name,age,gender):
+        self.name=name
+        self.age=age
+        self.gender=gender
+
+
+#method  create
+    def introduce(self):
+        return "my name is {}".format(self.name)
+
+
+
+person_one=Person("shakhaoyat",26,"male")
+
+print(person_one.name)
+print(person_one.age)
+print(person_one.gender)
+
+person_two=Person("chad",24,"male")
+print(person_two.name)
+print(person_two.age)
+print(person_two.gender)
+
+
+
+#methord call
+
+print(person_one.introduce())
+print(person_two.introduce())
