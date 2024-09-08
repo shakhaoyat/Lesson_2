33d0dac42a50e8238a0331715643f2c41da12eac
shakhaoyat <shakhaoyathossain.12@gmail.com>
2024-09-08 22:19:22 +0600

    1

--- udemy/input.py
+++ udemy/input.py
@@ -1,2 +1,13 @@
 name=input("what is your:")
 print("hello "+ name)
+
+
+value = int(input("Enter the value: "))
+
+if value <= 0:
+    absulate = -value
+else:
+    absulate = value
+
+print(absulate)
+
